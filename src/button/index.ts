38c3eb5bcbe3500398307abe4525c91074ac3d30
linzheng <alidlg9045@126.com>
2022-08-17 10:46:39 +0800

    运行一个简单的组件button

---
+++ src/button/index.ts
@@ -0,0 +1,12 @@
+import { defineComponent, h } from "vue";
+
+export default defineComponent({
+  name: "LzButton",
+
+  render() {
+
+    return h("button", null, "LzButton");
+
+  },
+
+});
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
 const s: string = 'Hello Typescript'
 console.log(s)
+
+import { createApp } from "vue";
+
+import SButton from "./button";
+
+createApp(SButton).mount("#app");
