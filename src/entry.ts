6218737ad336c84bc2d8645242442cb05e6cd8d1
linzheng <alidlg9045@126.com>
2022-08-19 18:43:02 +0800

    安装vuePress; vitepress-theme-demoblock插件; 实现 button 组件 展示在文档上的基本玩法

--- src/entry.ts
+++ src/entry.ts
@@ -5,7 +5,7 @@ import JSXButton from './JSXButton';
 import UnoButton from './button/index';
 
 // 导出单独组件
-export { MyButton, SFCButton, JSXButton,UnoButton };
+export { MyButton, SFCButton, JSXButton, UnoButton };
 
 // 编写一个插件，实现一个install方法
 export default {
