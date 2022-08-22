6996334f69fce0623f7784ba14246d7aa67a6e15
linzheng <alidlg9045@126.com>
2022-08-22 14:27:26 +0800

    修整 button 的 props

--- src/button/Button.tsx
+++ src/button/Button.tsx
@@ -1,38 +1,57 @@
 import { defineComponent, PropType, toRefs } from "vue";
 import "uno.css";
 
-export type IColor = 'black' | 'gray' | 'red' | 'yellow' | 'green' | 'blue' | 'indigo' | 'purple' | 'pink'
+export type BtnColor = 'black' | 'gray' | 'red' | 'yellow' | 'green' | 'blue' | 'indigo' | 'purple' | 'pink' | 'white'
+export type BtnSize = 'small' | 'medium' | 'normal'
 export const props = {
   color: {
-    type: String as PropType<IColor>,
-    default: 'blue'  // 设定默认颜色
+    type: String as PropType<BtnColor>,
+    default: 'white'  // 设定默认颜色
   },
   icon: {
     type: String as PropType<string>,
     default: ''
+  },
+  round: {
+    type: Boolean as PropType<boolean>,
+    default: false
+  },
+  circle: {
+    type: Boolean as PropType<boolean>,
+    default: false
+  },
+  size: {
+    type: String as PropType<BtnSize>,
+    default: 'normal'
   }
 }
+
 
 export default defineComponent({
   name: "UnoButton",
   props,
   setup(props, { slots }) {
+    const isColor = props.color !== 'white' ? true : false
+    const isMedium = props.size === 'medium' ? true : false
+    const isSmallOrMini = props.size === 'small' ? true : false
     return () => <button
       class={`
-        py-2 
-        px-4 
+        ${props.circle ? 'px-1' : 'px-4'}
         font-semibold 
-        rounded-lg 
+        ${props.round ? 'rounded-lg' : props.circle ? 'rounded-1/2' : ''}
         shadow-md 
-        text-white 
-        bg-${props.color}-500 
-        hover:bg-${props.color}-700 
+        ${isColor ? 'text-white' : 'text-black'}
+        ${isColor ? `bg-${props.color}-500` : `bg-${props.color}`}
+        ${isColor ? `hover:bg-${props.color}-700` : `hover:text-blue` }
+        ${isMedium ? 'text-4' : isSmallOrMini ? 'text-2' : ''}
+        ${isMedium ? 'py-2.5' : isSmallOrMini ? 'py-1' : 'py-2'}
+        ${isMedium ? 'px-4' : isSmallOrMini ? 'px-2.5' : ''}
         mx-1
         border-none 
         cursor-pointer
       `}
     >
-      {props.icon !== "" ? <i class={`i-ic-baseline-${props.icon} p-3`}></i> : ""}
+      {props.icon !== "" ? <i class={`i-ic-baseline-${props.icon} ${isSmallOrMini ? 'p-2' : 'p-3'}`}></i> : ""}
       {slots.default ? slots.default() : ''}
     </button>
   }
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,26 @@
-// const s: string = 'Hello Typescript'
-// console.log(s)
-
 import { createApp } from "vue/dist/vue.esm-browser";
 
-// import LzButton from "./button";
-import LzButton from "./LzButton.vue";
-import JSXButton from './JSXButton';
-import UnoButton from './button/index';
 import VueComponentUI from './entry';
 
 createApp({
   template:
     `
     <div>
-      <UnoButton color="blue">蓝色按钮</UnoButton>
+      <UnoButton>默认按钮</UnoButton>
+      <UnoButton color="white">白色按钮</UnoButton>
       <UnoButton color="green">绿色按钮</UnoButton>
       <UnoButton color="gray">灰色按钮</UnoButton>
       <UnoButton color="yellow">黄色按钮</UnoButton>
-      <UnoButton color="red">红色按钮</UnoButton>
-      <UnoButton color="blue" round plain icon="search" ></UnoButton>
-      <UnoButton color="green" round plain icon="edit" ></UnoButton>
-      <UnoButton color="gray" round plain icon="check" ></UnoButton>
-      <UnoButton color="yellow" round plain icon="message" ></UnoButton>
-      <UnoButton color="red" round plain icon="directions-run" ></UnoButton>
-    </div>
+      <UnoButton color="red" round>红色按钮</UnoButton>
+      <UnoButton color="blue" round icon="search">搜索</UnoButton>
+      <UnoButton color="green" circle icon="edit"></UnoButton>
+      </div>
+      <div style="margin-top: 20px">
+        <UnoButton size="small" >小号按钮</UnoButton>
+        <UnoButton size="small" icon="edit" >小号按钮</UnoButton>
+        <UnoButton>默认按钮</UnoButton>
+        <UnoButton size="medium" >大号按钮</UnoButton>
+      </div>
   `
 })
   .use(VueComponentUI)
